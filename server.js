5a62ce4a75b03a5b4dadb5f089d07cc3d6062667
JakobG <jakob.gamperling@web.de>
2019-05-20 19:00:07 +0200

    Hi

--- server.js
+++ server.js
@@ -16,4 +16,32 @@ app.listen(3000, function(){
 	console.log("listening on 3000");
 });
 
-// Fuck You
+app.use(express.static(__dirname + '/images'));
+
+app.get('/', (request, response) => {
+	response.render('anbieter');
+});
+
+app.get('/shirts', (request, response)=>{
+	response.render('shirts')
+});
+
+app.get('/pullis', (request, response)=>{
+	response.render('pullis')
+});
+
+app.get('/jacken', (request, response)=>{
+	response.render('jacken')
+});
+
+app.get('/hosen', (request, response)=>{
+	response.render('hosen')
+});
+
+app.get('/schuhe', (request, response)=>{
+	response.render('schuhe')
+});
+
+app.get('/accessoires', (request, response)=>{
+	response.render('accessoires')
+});
