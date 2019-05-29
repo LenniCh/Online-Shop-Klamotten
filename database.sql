cefa8df6ea75598531ea2670234828946b7f3787
LenniCh <49150829+LenniCh@users.noreply.github.com>
2019-05-29 11:57:56 +0200

    Thrasher-Page mit DB

---
+++ database.sql
@@ -0,0 +1,30 @@
+CREATE TABLE produkte ( 
+    produktID INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    kategorie TEXT NOT NULL,
+    s TEXT NOT NULL,
+    m TEXT NOT NULL,
+    l TEXT NOT NULL,
+    farbe TEXT NOT NULL,
+    preis NUMERIC NOT NULL,
+    anbieter TEXT NOT NULL,
+    bild TEXT NOT NULL
+
+    );
+
+
+
+INSERT INTO produkte(
+    name, kategorie, s, m, l, farbe, preis, anbieter, bild
+)
+VALUES (
+    "Hoodie", "Pullover", 1, 1, 1, "grau", 35.99, "Thrasher", "thrasherhoodie.jpg"
+);
+
+
+
+db.each('SELECT * FROM produkte', (err, row) => {
+	const produktID = row.produktID;     
+	const name = row.name;    
+	const preis = row.preis; 
+});
--- server.js
+++ server.js
@@ -7,9 +7,20 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.engine('.ejs', require('ejs').__express);
 app.set('view engine', 'ejs');
 
-// initialize SQLite with database 'shop.db'
-const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('shop.db');
+// initialize SQLite with database 'produkte.db'
+const sqlite3 = require('sqlite3').verbose(); 
+ 
+let db = new sqlite3.Database('produkte.db', (err) => {
+    if (err) {
+		console.error(err.message);   
+	}
+	 console.log('Connected to the produkte database.'); 
+}); 
+
+
+
+
+
 
 // Webserver starten http://localhost:3000
 app.listen(3000, function(){
@@ -25,4 +36,17 @@ app.get("/pullover", function(req,res){
 app.get("/thrasher", function(req,res){
 	res.render('thrasher');
 });
+app.get("/thrasher2", function(req,res){
+	let thrasher ="Thrasher"
+	db.all(`SELECT * FROM produkte WHERE anbieter='${thrasher}'`, function(err, rows){
+		if (err){
+			console.log(err.message);
+		}
+		res.render('thrasher2',{'produkte':rows || []});
+		
+	});
+});
+
+
 app.use(express.static(__dirname+'/pics'));
+app.use(express.static(__dirname+'/main'));
