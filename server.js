e7bdcb36ceaa47b600d032b05d126e478927c08d
SniBie <l.heindl20333@gmail.com>
2019-06-02 17:22:52 +0200

    fixed links etc.
    -missing nav bar -> login/reg
    -weird artikel ejs size

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express');
-const app = express()
-const bodyParser= require('body-parser')
+const app = express();
+
+const bcrypt = require('bcrypt');
+const saltRounds = 8;
+
 const fs = require('fs');
+
+const bodyParser= require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}))
 
 // initialize ejs template engine
 app.engine('.ejs', require('ejs').__express);
 app.set('view engine', 'ejs');
 
-// initialize SQLite with database 'users.db'
+// initialize SQLite with database 'produkte.db'
 const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('users.db');
+let db = new sqlite3.Database('produkte.db', (err) => {
+	if(err) {
+		console.error(err.message);
+	}
+	console.log('Connected to the produkte database.');
+});
 
 // Webserver starten http://localhost:3000
 app.listen(3000, function(){
@@ -20,7 +30,7 @@ app.listen(3000, function(){
 app.use(express.static(__dirname + '/images'));
 app.use(express.static(__dirname + '/stylings'));
 
-
+// Session initilialisieren
 const session = require('express-session');
 app.use(session({
 	secret: 'example',
@@ -31,6 +41,7 @@ app.use(session({
 app.get('/', (request, response) => {
 	let authenticated = request.session.authenticated;
 	let username = request.session.username;
+	var sessionVariable = request.session.authenticated;
 	
 	let greeting;
 	
@@ -42,12 +53,14 @@ app.get('/', (request, response) => {
 
 	response.render('home', {
 		isLoggedIn: authenticated,
-		greeting: greeting
+		greeting: greeting,
+		sessionVariable: request.session.authenticated
 	});
 
 });
 
 app.get('/login', (request, response) => {
+
     if (!request.session.authenticated) {
         response.render('login', {
             error: false
@@ -62,7 +75,7 @@ app.post('/login', (request, response) => {
 	let username = request.body.username;
 	let password = request.body.password;
 
-	database.get(`SELECT * FROM users WHERE username='${username}'`, function(error, now) {
+	db.get(`SELECT * FROM users WHERE username='${username}'`, function(error, row) {
 		if(error) {
 			console.log(error);
 			response.redirect('/login');
@@ -114,6 +127,8 @@ app.post('/register', (request, response) => {
 	let username = request.body.username;
 	let password = request.body.password;
 	let passwordConfirm = request.body.passwordConfirm;
+	let brand = request.body.Markenname;
+	let banner = request.body.banner;
 
 	if (password != passwordConfirm) {
 		response.render('register', {
@@ -122,7 +137,7 @@ app.post('/register', (request, response) => {
 		return;
 	}
 
-	database.get(`SELECT * FROM users WHERE username='${username}'`, function(error, now) {
+	db.get(`SELECT * FROM users WHERE username='${username}'`, function(error, row) {
 		if (error) {
 			console.log(error);
 			response.redirect('/register');
@@ -137,7 +152,7 @@ app.post('/register', (request, response) => {
 					return;
 				}
 
-				database.run(`INSERT INTO users (username, password) VALUES ('${username}', '${hash}')`, (error) => {
+				db.run(`INSERT INTO users (username, password, brand, banner) VALUES ('${username}', '${hash}','${brand}','${banner}')`, (error) => {
 					if (error) {
 						console.log(error);
 						response.redirect('/register');
@@ -159,7 +174,7 @@ app.post('/register', (request, response) => {
 
 // Versuchsfunktion zum checken ob Session aktiv
 /* function sessionCheck(request, response) {
-	if(!request.session.authenticated) {
+	if(request.session.authenticated == false) {
 		response.render('navbar');
 		console.log("render navbar");
 	} else {
@@ -179,7 +194,7 @@ app.post("/addProduct", function(request, response){
 	const l = request.body.l;
 	const farbe = request.body.farbe;
 	const preis = request.body.preis;
-	const anbieter = "ich";
+	const anbieter = request.session.username;
 	const bild = request.body.bildlink;
 	console.log(name);
 	console.log(kategorie);
@@ -202,53 +217,111 @@ app.post("/addProduct", function(request, response){
 });
 
 app.get('/anbieter', (request, response) => {
-	response.render('anbieter');
+	var sessionVariable = request.session.authenticated;
+	response.render('anbieter', {
+		sessionVariable: request.session.authenticated
+	});
+});
+
+app.get('/navbar', (request, response) => {
+	response.render('navbar');
 });
 
 app.get('/artikel', (request, response) => {
-	response.render('artikel');
+	var sessionVariable = request.session.authenticated;
+	response.render('artikel', {
+		sessionVariable: request.session.authenticated
+	});
 });
 
 app.get('/shirts', (request, response)=>{
-	response.render('shirts');
+	var sessionVariable = request.session.authenticated;
+	response.render('shirts', {
+		sessionVariable: request.session.authenticated
+	});
 });
 
 app.get('/pullis', (request, response)=>{
-	response.render('pullis');
+	var sessionVariable = request.session.authenticated;
+	response.render('pullis', {
+		sessionVariable: request.session.authenticated
+	});
 });
 
 app.get('/jacken', (request, response)=>{
-	response.render('jacken');
+	var sessionVariable = request.session.authenticated;
+	response.render('jacken', {
+		sessionVariable: request.session.authenticated
+	});
 });
 
 app.get('/hosen', (request, response)=>{
-	response.render('hosen');
+	var sessionVariable = request.session.authenticated;
+	response.render('hosen', {
+		sessionVariable: request.session.authenticated
+	});
 });
 
 app.get('/schuhe', (request, response)=>{
-	response.render('schuhe');
+	var sessionVariable = request.session.authenticated;
+	response.render('schuhe', {
+		sessionVariable: request.session.authenticated
+	});
 });
 
 app.get('/accessoires', (request, response)=>{
-	response.render('accessoires');
+	var sessionVariable = request.session.authenticated;
+	response.render('accessoires', {
+		sessionVariable: request.session.authenticated
+	});
 });
 
-app.get("/bs", function(req,res){
-	res.render('bs');
+app.get("/thrasher", function(request,response){
+	var sessionVariable = request.session.authenticated;
+	response.render('thrasher', {
+		sessionVariable: request.session.authenticated
+	});
 });
-app.get("/pullover", function(req,res){
-	res.render('pullover');
-});
-app.get("/thrasher", function(req,res){
-	res.render('thrasher');
-});
-app.get("/thrasher2", function(req,res){
+
+app.get("/thrasher2", function(request,response){
+	var sessionVariable = request.session.authenticated;
 	let thrasher ="Thrasher"
 	db.all(`SELECT * FROM produkte WHERE anbieter='${thrasher}'`, function(err, rows){
 		if (err){
 			console.log(err.message);
 		}
-		res.render('thrasher2',{'produkte':rows || []});
+		response.render('thrasher2', {
+			'produkte':rows || [],
+			sessionVariable: request.session.authenticated
+		});
 		
+	});
+});
+
+app.get("/bs", function(request,response){
+	var sessionVariable = request.session.authenticated;
+	let element ="ELEMENT"
+	db.all(`SELECT * FROM users WHERE brand='${element}'`, function(err, rows){
+		if (err){
+			console.log(err.message);
+		}
+		response.render('bs', {
+			'anbieter':rows || [],
+			sessionVariable: request.session.authenticated
+		});
+		
+	});
+});
+
+app.get("/element", function(request,response){
+	var sessionVariable = request.session.authenticated;
+	let element ="ELEMENT"
+	db.all(`SELECT * FROM users WHERE username='${element}'`, (err, rows) => {
+		const banner = rows.banner
+		const brand = rows.brand
+
+	});
+	response.render('element', {
+		sessionVariable: request.session.authenticated
 	});
 });
